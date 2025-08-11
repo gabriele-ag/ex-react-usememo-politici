3594aa01de703eb41586e90064170f2dcb5dcb69
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-08-11 17:49:04 +0200

    feat: milestone 2 done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 
 function Main() {
   const [politicians, setPoliticians] = useState([])
+  const [search, setSearch] = useState("")
 
   const fetchJson = async (url) => {
     const response = await fetch(url)
@@ -24,13 +25,30 @@ function Main() {
     getData();
   }, []);
 
+  const filteredPoliticians = useMemo(() => {
+      return politicians.filter(politicians => {
+      const isIncluded = politicians.name.toLowerCase().includes(search.toLowerCase())
+      const isInDescr = politicians.biography.toLowerCase().includes(search.toLowerCase())
+      return isIncluded || isInDescr
+    })  
+  }, [politicians, search])
+
 
 
   return (
     <section>
+      <h1 className="mb-40">Ecco la lista dei politici</h1>
+      <form className="mb-40" action="">
+        <input 
+        type="text" 
+        name="" id="" 
+        placeholder="Cerca per nome o biografia..."
+        onChange={(e) => setSearch(e.target.value)}/>
+        <button>Cerca</button>
+      </form>
       <div className="card-box">
         <ul className="card-flex">
-          {politicians.map((curElem, index) => (
+          {filteredPoliticians.map((curElem, index) => (
             <li key={index}>
               <div className="card-politicians">
                 <h3>{curElem.name}</h3>
